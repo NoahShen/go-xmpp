4dd20a1fc61247747129f5695655097acaa16b5e
NoahShen <Noah.Shen87@gmail.com>
2013-05-13 13:55:55 +0800

    supporint ping refacoter handler code

--- handler.go
+++ handler.go
@@ -1,23 +1,48 @@
 package xmpp
 
-import ()
+import (
+	"time"
+)
 
 type Handler interface {
 	GetHandleCh() chan *Event
+	// if time < 0, no timeout limit
+	GetEvent(time.Duration) *Event
 	Filter(*Event) bool
-	IsOnce() bool
+	IsOneTime() bool
 }
 
-type ChatHandler struct {
+type DefaultHandler struct {
 	handlCh chan *Event
 }
 
-func NewChatHandler() Handler {
-	return &ChatHandler{make(chan *Event)}
+func (self *DefaultHandler) GetHandleCh() chan *Event {
+	return self.handlCh
 }
 
-func (self *ChatHandler) GetHandleCh() chan *Event {
-	return self.handlCh
+func (self *DefaultHandler) GetEvent(d time.Duration) *Event {
+	if d < 0 {
+		event := <-self.handlCh
+		return event
+	} else {
+		select {
+		case event := <-self.handlCh:
+			return event
+		case <-time.After(d):
+		}
+	}
+	return nil
+}
+
+// Chathandler
+type ChatHandler struct {
+	DefaultHandler
+}
+
+func NewChatHandler() Handler {
+	c := &ChatHandler{}
+	c.handlCh = make(chan *Event)
+	return c
 }
 
 func (self *ChatHandler) Filter(event *Event) bool {
@@ -32,7 +57,36 @@ func (self *ChatHandler) Filter(event *Event) bool {
 	}
 	return false
 }
-
-func (self *ChatHandler) IsOnce() bool {
+func (self *ChatHandler) IsOneTime() bool {
 	return false
+}
+
+//Ping handler
+type IqIDHandler struct {
+	iqId string
+	DefaultHandler
+}
+
+func NewIqIDHandler(iqId string) Handler {
+	iqH := &IqIDHandler{}
+	iqH.handlCh = make(chan *Event)
+	iqH.iqId = iqId
+	return iqH
+}
+
+func (self *IqIDHandler) Filter(event *Event) bool {
+	if event.Type == Stanza {
+		stanza := event.Stanza
+		if stanza != nil {
+			switch stanza := stanza.(type) {
+			case *IQ:
+				return stanza.Id == self.iqId
+			}
+		}
+	}
+	return false
+}
+
+func (self *IqIDHandler) IsOneTime() bool {
+	return true
 }
--- utils.go
+++ utils.go
@@ -1,7 +1,11 @@
 package xmpp
 
 import (
+	"bytes"
+	"errors"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 func ToBareJID(jid string) string {
@@ -11,4 +15,34 @@ func ToBareJID(jid string) string {
 	}
 	bareJid := jid[0:i]
 	return bareJid
+}
+
+func GetDomain(jid string) (string, error) {
+	j := strings.TrimSpace(jid)
+	a := strings.SplitN(j, "@", 2)
+	if len(a) == 2 {
+		return a[1], nil
+	}
+	return "", errors.New("invalid jid!")
+}
+
+const alpha = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+
+func RandomString(l int) string {
+	var result bytes.Buffer
+	var temp string
+	for i := 0; i < l; {
+		c := randChar()
+		if c != temp {
+			temp = c
+			result.WriteString(temp)
+			i++
+		}
+	}
+	return result.String()
+}
+
+func randChar() string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return string(alpha[rand.Intn(len(alpha)-1)])
 }
